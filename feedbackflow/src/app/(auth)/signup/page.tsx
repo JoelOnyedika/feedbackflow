dd68d2cb15a75493858e6623cc1c75070136e112
JoelOnyedika <86019717+JoelOnyedika@users.noreply.github.com>
2024-05-11 23:56:11 +0000

    Done with sign up form, moved schema to another file

--- feedbackflow/src/app/(auth)/signup/page.tsx
+++ feedbackflow/src/app/(auth)/signup/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import { useForm, SubmitHandler } from "react-hook-form";
 import { z } from "zod";
 
 import { Button } from "@/components/ui/button";
@@ -15,86 +15,117 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import {useState} from "react";
+import {signupFormSchema} from "@/lib/types";
 
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-});
 const Signup = () => {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof signupFormSchema>>({
+    mode: "onChange",
+    resolver: zodResolver(signupFormSchema),
     defaultValues: {
       username: "",
+      email: "",
+      password: "",
     },
   });
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
+const onSubmit: SubmitHandler<z.infer<typeof signupFormSchema>> = (formData: any) => {
+    console.log(formData);
   }
+
+  const isLoading = form.formState.isSubmitting;
+const [submitError, setSubmitError] = useState("")
 
   return (
     <div className="w-full h-screen flex justify-center items-center my-4">
-      <div className="p-8 border border-solid rounded-md">
+      <div className="p-8 border border-solid rounded-md space-y-5">
+        <div>
+          <h1 className="text-2xl font-bold text-center">Signup</h1>
+          <div className="flex justify-center">
+            <span className="mr-2 text-center">Already a user</span>
+            <span className="text-blue-500 hover:underline font-bold">
+              Log in
+            </span>
+          </div>
+        </div>
+        <div>
+          <Button
+            variant="info"
+            className="flex justify-center w-full text-white"
+          >
+            Signup with Google
+          </Button>
+        </div>
         <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
+          <form
+            onChange={() => {
+              if (submitError) setSubmitError("");
+            }}
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-5"
+          >
+            <FormField
+              control={form.control}
+              disabled={isLoading}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Username" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    This is your public display name.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              disabled={isLoading}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="joejoe@email.com"
+                      {...field}
+                      type={"email"}
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    This is your public display name.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              disabled={isLoading}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input placeholder="" {...field} type={"password"} />
+                  </FormControl>
+                  <FormDescription>
+                    This is your public display name.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button className="w-full" type="submit" disabled={isLoading}>
+              Submit
+            </Button>
+          </form>
+        </Form>
       </div>
-      
     </div>
-    
   );
 };
 
---
+++ feedbackflow/src/lib/types.ts
@@ -0,0 +1,25 @@
+import { z } from "zod";
+
+export const signupFormSchema = z.object({
+  username: z.string(). describe('Username').min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+  email: z.string(). describe("Email").email({
+    message: "Email is not valid.",
+  }),
+  password: z.string(). describe("Password").min(8, {
+    message: "Password must be at least 8 characters.",
+  }),
+});
+
+export const LoginFormSchema = z.object({
+  username: z.string(). describe('Username').min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+  email: z.string(). describe("Email").email({
+    message: "Email is not valid.",
+  }),
+  password: z.string(). describe("Password").min(8, {
+    message: "Password must be at least 8 characters.",
+  }),
+});
